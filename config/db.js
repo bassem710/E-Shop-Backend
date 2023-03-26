e13f5c89ba6a1bbf6c2a08519d8c078d52c395d5
Bassem <bassemsaeed620@gmail.com>
2023-03-26 14:26:08 +0200

    db edit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('colors');
 
-const dbConnection = _ => {
+const dbConnection = () => {
     mongoose.connect(process.env.MONGO_URI)
     .then( conn => console.log(`Database Connected: ${conn.connection.host}`.green.bold));
 }
