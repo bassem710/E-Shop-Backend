9a833517034438427b8e41a17aef339a83b07326
Bassem <bassemsaeed620@gmail.com>
2023-04-14 09:42:25 +0200

    coupon routes & server routes mount

---
+++ models/couponModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const couponSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: [true, "Coupon name is required"],
+        unique: true
+    },
+    expire: {
+        type: Date,
+        required: [true, "Coupon expiration date is required"]
+    },
+    discount: {
+        type: Number,
+        required: [true, "Coupon discount value is required"]
+    },
+}, {timestamps: true}); 
+
+const CouponModel = mongoose.model('Coupon', couponSchema);
+
+module.exports = CouponModel;
